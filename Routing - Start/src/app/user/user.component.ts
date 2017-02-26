39f4d411403a9472ef21e1d9e3635ff2ba644c5f
Madhur <madhur.ahuja@helpchat.in>
2017-02-26 15:08:44 +0530

    absolute and relative routes

--- Routing - Start/src/app/user/user.component.ts
+++ Routing - Start/src/app/user/user.component.ts
@@ -4,6 +4,8 @@ import { Component, OnDestroy } from '@angular/core';
   selector: 'app-user-component',
   template: `
       <h1>User Component</h1>
+      <a [routerLink]="['/user']">User</a>
+      <a [routerLink]="['../']">Home</a>
     `
 })
 export class UserComponent {
