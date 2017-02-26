5b5e1a5c4ebd47e86616eeacd6fbb1f61169db0f
Madhur <madhur.ahuja@helpchat.in>
2017-02-27 00:00:35 +0530

    recipe form prep

--- recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,14 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {RecipeService} from '../recipe.service';
+import {Subscription} from "rxjs/RX";
 
 @Component({
   selector: 'rb-recipe-edit',
   templateUrl: './recipe-edit.component.html',
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subscription: Subscription;
+  private recipeIndex: number;
+
+  constructor(
+    private route: ActivatedRoute, 
+    private sls: RecipeService
+    ) { }
 
   ngOnInit() {
+    let isNew = true;
+
+    this.subscription = this.route.params.subscribe(
+      (params:any) => {
+        if(params.hasOwnProperty('id')) {
+          isNew = false;
+           this.recipeIndex = +params['id']
+        }
+        else {
+          isNew = true;
+        }
+      }
+      
+    );
+  }
+
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
