88db23fcf906a3240f506341b16b0941431a3ccb
Madhur <madhur.ahuja@helpchat.in>
2017-02-26 16:22:19 +0530

    activate guard

---
+++ Routing - Start/src/app/user/user-detail.guard.ts
@@ -0,0 +1,12 @@
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from "@angular/router";
+import {Observable} from "rxjs/Rx";
+
+export class UserDetailGuard implements CanActivate {
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>  | boolean {
+
+        return confirm('Are you sure?');
+        //return false;
+    }
+
+}
