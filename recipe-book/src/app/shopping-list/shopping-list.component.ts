d9ccc365e98d94cf692926e6d6d3f5e344ad9cf6
Madhur <madhur.ahuja@helpchat.in>
2017-02-26 13:16:08 +0530

    add shopping list

--- recipe-book/src/app/shopping-list/shopping-list.component.ts
+++ recipe-book/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Ingredient} from '../shared/ingredient'
+import {ShoppingListService} from './shopping-list.service'
+
 
 @Component({
   selector: 'rb-shopping-list',
@@ -10,9 +12,11 @@ export class ShoppingListComponent implements OnInit {
   items: Ingredient[] = [];
   
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
+
+    this.items = this.sls.getItems();
   }
 
 }
