38f8e30f87c69be3d21e7c86cea2d79cf136d5c4
Madhur <madhur.ahuja@helpchat.in>
2017-02-26 14:18:00 +0530

    minor changes

--- recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Recipe} from '../recipe';
+import {ShoppingListService} from '../../shopping-list/shopping-list.service'
 
 @Component({
   selector: 'rb-recipe-detail',
@@ -8,7 +9,13 @@ import {Recipe} from '../recipe';
 export class RecipeDetailComponent implements OnInit {
   @Input() selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
+
+  onAddShoppingList() {
+    console.log(this.selectedRecipe.ingredients);
+    //alert('clicked');
+    this.sls.addItems(this.selectedRecipe.ingredients);
+  }
 
   ngOnInit() {
   }
--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Recipe } from './recipe'
-
+import { Ingredient } from '../shared/ingredient'
 @Injectable()
 export class RecipeService {
 
   private recipes: Recipe[] = [
-      new Recipe('Chutney', 'good chutney', 'http://www.vegrecipesofindia.com/wp-content/uploads/2013/03/coriander-chutney-recipe.jpg', []),
-      new Recipe('Chicken', 'good chicken' , 'http://www.pngmart.com/files/2/Cooked-Chicken-PNG-Transparent-Image.png', [])
+      new Recipe('Chutney', 'good chutney', 'http://www.vegrecipesofindia.com/wp-content/uploads/2013/03/coriander-chutney-recipe.jpg', [
+          new Ingredient('French Fries', 2),
+          new Ingredient('Pork Meat', 2)
+
+      ]),
+      new Recipe('Chicken', 'good chicken' , 'http://www.pngmart.com/files/2/Cooked-Chicken-PNG-Transparent-Image.png', [
+        new Ingredient('French Fries', 2)
+      ])
   ];
   
   constructor() { }
