4e16040e7f6c68749110945a724b98c2da51bd1a
Madhur <madhur.ahuja@helpchat.in>
2017-02-26 15:11:40 +0530

    routing through code

--- Routing - Start/src/app/user/user.component.ts
+++ Routing - Start/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-user-component',
@@ -6,7 +7,14 @@ import { Component, OnDestroy } from '@angular/core';
       <h1>User Component</h1>
       <a [routerLink]="['/user']">User</a>
       <a [routerLink]="['../']">Home</a>
+      <button (click)="onNavigate()">Go Home</button>
     `
 })
 export class UserComponent {
+  constructor(private router: Router) {
+
+  }
+  onNavigate() {
+    this.router.navigate(['/']);
+  }
 }
