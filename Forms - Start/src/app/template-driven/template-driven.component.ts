340fcef6ad7c0c47247b453e8eed54cf8117c4ed
Madhur <madhur.ahuja@helpchat.in>
2017-02-26 19:50:31 +0530

    fetching values

--- Forms - Start/src/app/template-driven/template-driven.component.ts
+++ Forms - Start/src/app/template-driven/template-driven.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'template-driven',
   templateUrl: 'template-driven.component.html'
 })
 export class TemplateDrivenComponent {
+
+  onSubmit(form: NgForm) {
+    console.log("It works");
+    console.log(form);
+  }
 }
