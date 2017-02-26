3a457812f22fb56aab75160ce75755be741e8074
Madhur <madhur.ahuja@helpchat.in>
2017-02-26 16:04:09 +0530

    default routes

--- Routing - Start/src/app/app.routing.ts
+++ Routing - Start/src/app/app.routing.ts
@@ -4,10 +4,10 @@ import {UserComponent} from "./user/user.component";
 import {USER_ROUTES} from './user/user.routes';
 
 const APP_ROUTES: Routes = [
+    { path: 'user', redirectTo: '/user/1', pathMatch: 'full' },
     { path: 'user/:id', component: UserComponent, children: USER_ROUTES},
-    { path: '', component: HomeComponent}
-
-
+    { path: '', component: HomeComponent},
+    { path: '**', redirectTo: '/user/1'}
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
