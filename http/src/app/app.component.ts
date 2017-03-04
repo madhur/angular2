1fcd11886313792d2f9750a95fbfdf07d6aa4646
Madhur <madhur.ahuja@helpchat.in>
2017-03-04 08:25:35 +0530

    add http project

---
+++ http/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpService} from './http.service';
+import {Response} from '@angular/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  providers: [HttpService]
+})
+export class AppComponent implements OnInit {
+
+  constructor(private httpService: HttpService){}
+
+  ngOnInit() {
+    this.httpService.getData().subscribe(
+      (data: Response) => console.log(data)
+    );
+  }
+} 
